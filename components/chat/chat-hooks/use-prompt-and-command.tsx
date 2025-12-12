63c0abf6caf552c7082be59c2e38cb2a3438ed9c
IndraPangestuu <matiasindrapangestu@gmail.com>
2025-12-13 03:21:43 +0700

    fix build

--- components/chat/chat-hooks/use-prompt-and-command.tsx
+++ components/chat/chat-hooks/use-prompt-and-command.tsx
@@ -25,6 +25,7 @@ export const usePromptAndCommand = () => {
     setAtCommand,
     setIsAssistantPickerOpen,
     setSelectedAssistant,
+    chatSettings,
     setChatSettings,
     setChatFiles
   } = useContext(ChatbotUIContext)
